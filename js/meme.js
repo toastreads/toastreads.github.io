378fabbc93fe4e3ceaced8e3f5d6c4b74f9798c2
nithinpublic <nithinpublic@gmail.com>
2024-09-02 02:14:34 +0100

    meme

--- js/meme.js
+++ js/meme.js
@@ -1,18 +1,28 @@
-function fetch_and_write("https://meme-api.com/gimme") {
+function fetch_meme() {
     console.log("Fetching meme...")
-    fetch(https://meme-api.com/gimme)
+    fetch("https://meme-api.com/gimme")
         .then(response => {
             if (response.ok) {
-                console.log("Success")
+                console.log("Meme fetch Success")
                 return response.json()
 
 
             } else {
-                console.log("Fetch failed")
+                console.log("Meme Fetch failed")
                 return response.json()
             }
         })
         .then(data => {
             write_meme(data)
+            console.log(data);
+            
         })
     }
+
+    function write_meme(jsonData) {
+        console.log("Meme selected:", jsonData.preview[1]);
+        
+        document.getElementById("meme-pic").style.backgroundImage = String("url(\"" + jsonData.preview[1] + "\"");
+    }
+
+    document.onload = fetch_meme()
