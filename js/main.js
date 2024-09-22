ece532dc5831f20c709a53e586a8ced1c5f3133b
nithinpublic <nithinpublic@gmail.com>
2024-09-22 20:02:43 +0100

    m

--- js/main.js
+++ js/main.js
@@ -300,6 +300,7 @@ function setup_done_button() {
 function setup_note_button() {
     document.getElementById('note-button').addEventListener('click', function (event) {
         snack("Notes coming soon!")
+        document.querySelector(".notepad-container").style.display = "block"
     })
 }
 
