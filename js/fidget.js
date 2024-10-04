5666907b927afdbac891e659d076ab7dee2f1d8c
nithinpublic <nithinpublic@gmail.com>
2024-10-04 22:02:28 +0100

    m

--- js/fidget.js
+++ js/fidget.js
@@ -1,8 +1,10 @@
-// new Propeller(document.getElementById('fidget-spinner'), {
-//     inertia: 0.992, speed: 0,
-//     angle: 0
-// });
 new Propeller(document.getElementById('fidget-spinner'), {
-    inertia: 0.9999, speed: 0, step:50,
+    inertia: 0.9999, speed: 0,
     angle: 0
 });
+
+
+// new Propeller(document.getElementById('fidget-spinner'), {
+//     inertia: 0.9999, speed: 0, step:50,
+//     angle: 0
+// });
