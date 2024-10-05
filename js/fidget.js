76537889c42c9072cf5c72cc43728cf225612846
nithinpublic <nithinpublic@gmail.com>
2024-10-05 02:32:02 +0100

    m

--- js/fidget.js
+++ js/fidget.js
@@ -8,3 +8,12 @@ new Propeller(document.getElementById('fidget-spinner'), {
 //     inertia: 0.9999, speed: 0, step:50,
 //     angle: 0
 // });
+
+fidgetPhenaSwitchButton = document.querySelector("#fidget-phena-switch-button")
+fidgetPhenaSwitchButton.addEventListener("click", () => {
+
+    new Propeller(document.getElementById('phena-spinner'), {
+        inertia: 0.9999, speed: 0, step: 29,
+        angle: 0
+    });
+})
